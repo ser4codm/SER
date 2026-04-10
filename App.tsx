773ce2e13f8bb955afd89802a959087fd5b1acba
ser4codm <ser4codm@gmail.com>
2026-04-11 02:41:41 +0300

    App.tsx

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
 import { Switch, Route, Router as WouterRouter } from "wouter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { Home } from "@/pages/Home";
-import { Search } from "@/pages/Search";
-import { Watchlist } from "@/pages/Watchlist";
-import { Recent } from "@/pages/Recent";
-import { TVDetails } from "@/pages/Details";
-import { MoviePlayer, TVPlayer } from "@/pages/Player";
-import NotFound from "@/pages/not-found";
+import { Toaster } from "./components/ui/toaster";
+import { TooltipProvider } from "./components/ui/tooltip";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Watchlist from "./pages/Watchlist";
+import Recent from "./pages/Recent";
+import TVDetails from "./pages/Details";
+import CinemaPlayer from "./CinemaPlayer"; 
+import NotFound from "./pages/not-found";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -29,8 +29,9 @@ function Router() {
       <Route path="/watchlist" component={Watchlist} />
       <Route path="/recent" component={Recent} />
       <Route path="/tv/:id" component={TVDetails} />
-      <Route path="/play/movie/:id" component={MoviePlayer} />
-      <Route path="/play/tv/:id/:season/:episode" component={TVPlayer} />
+      {/* ركز هنا يا بطل، الحين ربطنا المشغل اللي رفعناه بالروابط صح */}
+      <Route path="/play/movie/:id" component={CinemaPlayer} />
+      <Route path="/play/tv/:id/:season/:episode" component={CinemaPlayer} />
       <Route component={NotFound} />
     </Switch>
   );
@@ -40,11 +41,13 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
+        <WouterRouter base={""}>
           <Router />
+          <Toaster />
         </WouterRouter>
-        <Toaster />
       </TooltipProvider>
     </QueryClientProvider>
   );
 }
+
+export default App;
